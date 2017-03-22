47961f9fb27c496b7dbb6bd99974580dc656ce11
Michael Savastio <Michael.Savastio@VisibleWorld.com>
2017-03-22 16:31:19 -0400

    Added important TODO's.

--- src/Anamnesis.jl
+++ src/Anamnesis.jl
@@ -6,6 +6,8 @@ using DataUtils
 # TODO write forget macros
 # TODO implement size limits for hashing
 # TODO handle different orderings of keyword arguments
+# TODO implement metadata as HDF5, so you don't have to reload the whole damn thing
+# TODO consider using HDF5 or JLD for main storage (how would this work?)
 
 
 const METADATA_FILENAME = "metadata.jbin"
